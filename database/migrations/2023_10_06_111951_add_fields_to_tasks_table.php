cc759dc1a6a355dd983e792a7797dfcf448f6d01
Jose Luis Trevizon <jltrevizon@gmail.com>
2023-10-06 07:33:45 -0400

    fix

---
+++ database/migrations/2023_10_06_111951_add_fields_to_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFieldsToTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->boolean('costo')->default(false)->after('duration');
+            $table->timestamp('deleted_at')->nullable();
+            $table->unsignedBigInteger('created_by')->after('created_at')->nullable();
+            $table->foreign('created_by')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            //
+        });
+    }
+}
